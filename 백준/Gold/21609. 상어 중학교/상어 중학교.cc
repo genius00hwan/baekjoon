e74c64396a931bde51db098da9c6bb76b9b119d7
김수환 <101265039+genius00hwan@users.noreply.github.com>
2024-08-04 17:46:29 +0900

    [Gold II] Title: 상어 중학교, Time: 4 ms, Memory: 2164 KB -BaekjoonHub

--- 백준/Gold/21609. 상어 중학교/상어 중학교.cc
+++ 백준/Gold/21609. 상어 중학교/상어 중학교.cc
@@ -4,9 +4,9 @@ using namespace std;
 
 int n, m;
 int ans;
-int graph[21][21];
-int tmp[21][21];
-bool chk[21][21];
+int graph[22][22];
+int tmp[22][22];
+bool chk[22][22];
 int dr[4] = {1, -1, 0, 0};
 int dc[4] = {0, 0, 1, -1};
 bool flag = true;
@@ -14,12 +14,13 @@ bool flag = true;
 
 struct bg {
     int r, c;
+    int bn;
     int rn;
-    vector<pair<int, int>> bs;
+};
 
-
+struct compare {
     bool operator()(bg g1, bg g2) {
-        if (g1.bs.size() == g2.bs.size()) {
+        if (g1.bn == g2.bn) {
             if (g1.rn == g2.rn) {
                 if (g1.r == g2.r)
                     return g1.c < g2.c;
@@ -27,7 +28,7 @@ struct bg {
             }
             return g1.rn < g2.rn;
         }
-        return g1.bs.size() < g2.bs.size();
+        return g1.bn < g2.bn;
     }
 };
 
@@ -43,7 +44,8 @@ void print() {
 }
 
 bool oob(int r, int c) {
-    return (r < 0 || r >= n || c < 0 || c >= n);
+    if (r < 0 || r >= n || c < 0 || c >= n)return true;
+    return false;
 }
 
 void rotate() {
@@ -61,9 +63,11 @@ void rotate() {
 
 void gravity(int c) {
     for (int r = n - 2; r >= 0; r--) {
-        if (graph[r][c] < 0)continue;
+        if (graph[r][c] == -1)continue;
+        if (graph[r][c] == -2)continue;
         int cr = r;
         int nr = cr + 1;
+        if (oob(nr, c))continue;
         while (graph[nr][c] == -2) {
             graph[nr][c] = graph[cr][c];
             graph[cr][c] = -2;
@@ -80,11 +84,11 @@ bg bfs(int r, int c) {
             if (!graph[i][j])chk[i][j] = false;
         }
     }
-    bg ret = {r, c, 0};
+    bg ret = {r, c, 1};
+
     queue<pair<int, int>> q;
     q.push({r, c});
     chk[r][c] = true;
-    ret.bs.emplace_back(r, c);
     while (!q.empty()) {
         int cr = q.front().first;
         int cc = q.front().second;
@@ -98,7 +102,7 @@ bg bfs(int r, int c) {
             if (!graph[nr][nc])ret.rn++;
             q.push({nr, nc});
             chk[nr][nc] = true;
-            ret.bs.emplace_back(nr, nc);
+            ret.bn++;
         }
     }
     return ret;
@@ -113,40 +117,67 @@ void init() {
     }
 }
 
+void erase(bg f) {
+    memset(chk, false, sizeof chk);
+
+    queue<pair<int, int>> q;
+    vector<pair<int, int>> v;
+
+    q.push({f.r, f.c});
+    chk[f.r][f.c] = true;
+    v.emplace_back(f.r, f.c);
+    while (!q.empty()) {
+        int cr = q.front().first;
+        int cc = q.front().second;
+        q.pop();
+        for (int i = 0; i < 4; i++) {
+            int nr = cr + dr[i];
+            int nc = cc + dc[i];
+            if (oob(nr, nc))continue;
+            if (chk[nr][nc])continue;
+            if (graph[nr][nc] && graph[nr][nc] != graph[f.r][f.c])continue;
+            q.push({nr, nc});
+            chk[nr][nc] = true;
+            v.emplace_back(nr, nc);
+        }
+    }
+    for (pair<int, int> p: v) {
+        graph[p.first][p.second] = -2;
+    }
+}
+
 void play() {
-    priority_queue<bg, vector<bg>, bg> groups;
+    priority_queue<bg, vector<bg>, compare> groups;
     memset(chk, false, sizeof chk);
     for (int i = 0; i < n; i++) {
         for (int j = 0; j < n; j++) {
-            if (chk[i][j])continue;
-            if (graph[i][j] <= 0)continue;
+            if (chk[i][j])
+                continue;
+            if (graph[i][j] <= 0)
+                continue;
             groups.push(bfs(i, j));
         }
     }
-    
-    if (groups.empty() || groups.top().bs.size() <= 1) {
+    if (groups.empty() || groups.top().bn <= 1) {
         flag = false;
         cout << ans;
         return;
     }
 
     bg f = groups.top();
-    ans += f.bs.size() * f.bs.size();
-    for (auto p: f.bs) {
-        graph[p.first][p.second] = -2;
-    }
+    ans += f.bn * f.bn;
+    erase(f);
 //    print();
 
-    for (int i = 0; i < n; i++) {
-        gravity(i);
+    for (int j = 0; j < n; j++) {
+        gravity(j);
     }
 //    print();
 
     rotate();
-//    print();
 
-    for (int i = 0; i < n; i++) {
-        gravity(i);
+    for (int j = 0; j < n; j++) {
+        gravity(j);
     }
 //    print();
 
