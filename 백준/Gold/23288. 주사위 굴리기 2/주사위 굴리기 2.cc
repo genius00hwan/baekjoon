2003ba93fdf84e6fd74693d086c161634a4d7d7d
김수환 <101265039+genius00hwan@users.noreply.github.com>
2024-08-18 20:20:36 +0900

    [Gold III] Title: 주사위 굴리기 2, Time: 4 ms, Memory: 2028 KB -BaekjoonHub

--- 백준/Gold/23288. 주사위 굴리기 2/주사위 굴리기 2.cc
+++ 백준/Gold/23288. 주사위 굴리기 2/주사위 굴리기 2.cc
@@ -5,6 +5,8 @@ using namespace std;
 int n, m, k;
 int graph[21][21];
 bool chk[21][21];
+
+// 동 남 서 북
 int dr[] = {0, 1, 0, -1};
 int dc[] = {1, 0, -1, 0};
 
@@ -16,11 +18,18 @@ bool oob(int r, int c) {
 class dice {
 public:
     int front, up, right;
+    // 앞면으로 뒷면이, 윗면으로 아랫면이, 오른쪽 면으로 왼쪽 면이 결정된다.
     int r, c;
+    // 현재 죄표를 저장하여, 이동하고, 점수를 계산한다.
+
     int dir;
+    // 방향을 저장하여 이동한다.
+
     int point;
+    // 점수를 저장하고 마지막에 출력한다.
 
     dice(int, int, int, int, int, int);
+    // 초기값.
 
     void goEast();
 
@@ -30,11 +39,27 @@ public:
 
     void goNorth();
 
+    /**
+     * 동서남북 4방중 1로 이동할 수 있다.
+     */
+
     void move();
 
+    /**
+     * 정해져 있는 방향에 맞게 이동한다.
+     */
     void turn();
 
+    /**
+     * 방향을 결정한다.
+     */
+
+
     int getValue();
+
+    /**
+     * 점수를 계산할때는 밑면을 기준으로 한다.
+     */
 
     void addPoint();
 };
@@ -85,6 +110,12 @@ dice::dice(int front, int up, int right, int r, int c, int dir) {
     this->dir = dir;
 }
 
+/**
+ * 동쪽으로 이동 : F <- F, U <- L(7-R), R <- U
+ * 남쪽으로 이동 : F <- U, U <-B(7-F), R <- R
+ * 서쪽으로 이동 : F <- F, U <- R, R <- D(7-U)
+ * 북쪽으로 이동 : F <- D(7-U), U <-F, R <- R
+ */
 void dice::goEast() {
     int oU = up;
     int oR = right;
@@ -113,9 +144,12 @@ void dice::goNorth() {
     up = oF;
 }
 
+// 방향을 보고 동서남북 중 1로 이동
 void dice::move() {
     if (oob(r + dr[dir], c + dc[dir]))
         dir = (dir + 2) % 4;
+    // 범위를 벗어나면 반대 방향
+
 
     if (dir == 0) goEast();
     else if (dir == 1)goSouth();
@@ -126,6 +160,15 @@ void dice::move() {
     c = c + dc[dir];
 }
 
+/**
+ *   그래프에 있는 값 (A)과 주사위 아랫면 (B) 을 비교하고 회전
+ *  A < B : 반시계 방향 (dir+=1)
+ *  A > B : 시계 방향 (dir-=1)
+ *  
+ *  동 남 서 북
+ *  dr[] = {0, 1, 0, -1}
+*   dc[] = {1, 0, -1, 0}
+ */
 void dice::turn() {
     if (graph[r][c] < getValue())
         dir = (dir + 1) % 4;
@@ -136,15 +179,20 @@ void dice::turn() {
 
 
 int dice::getValue() {
+    // 밑면을 반환
     return 7 - up;
 }
 
+/**
+ * bfs를 돌리면서 같은값을 찾아서 갯수를 구한다.
+ */
+
 void dice::addPoint() {
     memset(chk, false, sizeof chk);
-    int std = graph[r][c];
-    int cnt = 1;
+    // visited 배열을 전역변수로 선언해둠
     queue<pair<int, int>> q;
     q.push({r, c});
+    point += graph[r][c];
     chk[r][c] = true;
     while (!q.empty()) {
         pair<int, int> cur = q.front();
@@ -154,12 +202,11 @@ void dice::addPoint() {
             int nc = cur.second + dc[i];
             if (oob(nr, nc))continue;
             if (chk[nr][nc])continue;
-            if (graph[nr][nc] != std)continue;
-            cnt++;
+            if (graph[nr][nc] != graph[r][c])continue;
+            point += graph[nr][nc];
             q.push({nr, nc});
-            chk[nr][nc]= true;
+            chk[nr][nc] = true;
         }
     }
-    point += (cnt * std);
 
 }
