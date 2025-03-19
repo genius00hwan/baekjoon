675229cd4cc7f6f93883840038304763e9885a5c
김수환 <101265039+genius00hwan@users.noreply.github.com>
2025-03-20 02:52:12 +0900

    [Gold III] Title: 일감호에 다리 놓기, Time: 300 ms, Memory: 12204 KB -BaekjoonHub

--- 백준/Gold/17490. 일감호에 다리 놓기/일감호에 다리 놓기.cc
+++ 백준/Gold/17490. 일감호에 다리 놓기/일감호에 다리 놓기.cc
@@ -1,76 +1,63 @@
-//
-// Created by 김수환 on 2022/09/02.
-//
-#include <iostream>
-#include <set>
-#include <map>
-#include <vector>
-#include <algorithm>
+#include <bits/stdc++.h>
 
 using namespace std;
 
 int n, m;
-long long cnt, k;
-vector<pair<int, pair<int, int>>> edges;
-set<pair<int, int>> dmz;
-int par[1000005];
+long long k, ans;
+int rock[1'000'002];
+vector<int> disable;
 
-void reset() {
-    for (int i = 1; i <= n; i++)
-        par[i] = i;
+void init() {
+    cin >> n >> m >> k;
+
+    for (int i = 1; i <= n; i++) {
+        cin >> rock[i];
+    }
+
+    for (int i = 0; i < m; i++) {
+        int a, b;
+        cin >> a >> b;
+        if ((a == n && b == 1) || (b == n && a == 1))
+            disable.push_back(n);
+        else
+            disable.push_back(min(a, b));
+    }
+    sort(disable.begin(), disable.end());
 }
 
-int find(int x) {
-    if (par[x] == x)
-        return x;
-    else
-        return par[x] = find(par[x]);
+int cal(int start, int end) {
+    int cost = INT_MAX;
+    for (int i = start; i <= end; i++) {
+        cost = min(cost, rock[i]);
+    }
+    return cost;
 }
 
-void merge(int x, int y) {
-    x = find(x);
-    y = find(y);
+void solve() {
+    if (m <= 1) {
+        cout << "YES\n";
+        exit(0);
+    }
+    int prev = 1;
 
-    par[y] = x;
+    for (int cur : disable) {
+        int mc = cal(prev, cur);
+
+        if (cur == disable.front()) {
+            mc = min(mc, cal(disable.back() + 1, n));
+        }
+
+        ans += mc;
+        prev = cur + 1;
+    }
+
+    cout << (ans <= k ? "YES\n" : "NO\n");
 }
 
 int main() {
     ios::sync_with_stdio(false);
-    cin.tie(0);
+    cin.tie(nullptr);
 
-    cin >> n >> m >> k;
-
-    for (int i = 1; i <= n; i++) {
-        int w;
-        cin >> w;
-        edges.push_back({w, {n, i % n}});
-    }
-    for (int i = 0; i < m; i++) {
-        int u, v;
-        cin >> u >> v;
-        dmz.insert({u % n, v % n});
-    }
-    for (int i = 1; i <= n; i++) {
-        if (dmz.find({i % n, (i + 1) % n}) != dmz.end()) continue;
-        if (dmz.find({(i + 1) % n, i % n}) != dmz.end()) continue;
-        edges.push_back({0, {i % n, (i + 1) % n}});
-    }
-    reset();
-    sort(edges.begin(), edges.end());
-    for (auto edge: edges) {
-        long long w = edge.first;
-        int from = edge.second.first;
-        int to = edge.second.second;
-
-        if (find(from) != find(to)) {
-            merge(from, to);
-            cnt += w;
-        }
-    }
-    if (m <= 1) cout << "YES" << '\n';
-    else {
-        if (cnt > k)
-            cout << "NO" << '\n';
-        else cout << "YES" << '\n';
-    }
+    init();
+    solve();
 }
