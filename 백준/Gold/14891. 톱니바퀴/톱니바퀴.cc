db50b00be4cacf37b2e7cee4015ba427ae0a3e3e
김수환 <101265039+genius00hwan@users.noreply.github.com>
2025-03-30 01:20:43 +0900

    [Gold V] Title: 톱니바퀴, Time: 0 ms, Memory: 2028 KB -BaekjoonHub

--- 백준/Gold/14891. 톱니바퀴/톱니바퀴.cc
+++ 백준/Gold/14891. 톱니바퀴/톱니바퀴.cc
@@ -14,15 +14,28 @@ int ans;
 int chk[N]; // -1 가만히 , 1 시계, 반시계 0
 
 void rot(deque<bool> *w, bool dir) {
-    if (dir) {
-        bool v = w->back();
+    if (dir) { // 시계
+        w->push_front(w->back());
         w->pop_back();
-        w->push_front(v);
-        return;
+    } else { // 반시계
+        w->push_back(w->front());
+        w->pop_front();
     }
-    bool v = w->front();
-    w->pop_front();
-    w->push_back(v);
+}
+
+
+void chk_left(int idx, bool dir) {
+    if (!idx)return; // 가장 왼쪽
+    if (wheels[idx][L] == wheels[idx - 1][R])return;
+    chk[idx - 1] = !dir;
+    chk_left(idx - 1, !dir);
+}
+
+void chk_right(int idx, bool dir) {
+    if (idx == N - 1)return; // 가장 오른쪽
+    if (wheels[idx][R] == wheels[idx + 1][L])return;
+    chk[idx + 1] = !dir;
+    chk_right(idx + 1, !dir);
 }
 
 void init() {
@@ -41,27 +54,14 @@ void init() {
     }
 }
 
-void cal_left(int idx, bool dir) {
-    if (!idx)return; // 가장 왼쪽
-    if (wheels[idx][L] == wheels[idx - 1][R])return;
-    chk[idx - 1] = !dir;
-    cal_left(idx - 1, !dir);
-}
-
-void cal_right(int idx, bool dir) {
-    if (idx == N - 1)return; // 가장 오른쪽
-    if (wheels[idx][R] == wheels[idx + 1][L])return;
-    chk[idx + 1] = !dir;
-    cal_right(idx + 1, !dir);
-}
 void solve() {
     for (auto c: cmd) {
         memset(chk, -1, sizeof chk);
         vector<pair<int, bool>> rw;
 
         chk[c.first] = c.second;
-        cal_left(c.first, c.second);
-        cal_right(c.first, c.second);
+        chk_left(c.first, c.second);
+        chk_right(c.first, c.second);
         for (int i = 0; i < N; i++) {
             if (chk[i] == -1)continue;
             rw.emplace_back(i, chk[i]);
