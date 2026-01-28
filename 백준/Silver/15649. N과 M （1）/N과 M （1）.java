dc119aeeb93804160f910f3ab90c7e0bd78200e1
김수환 <101265039+genius00hwan@users.noreply.github.com>
2026-01-28 17:35:05 +0900

    [Silver III] Title: N과 M (1), Time: 228 ms, Memory: 23032 KB -BaekjoonHub

--- 백준/Silver/15649. N과 M （1）/N과 M （1）.java
+++ 백준/Silver/15649. N과 M （1）/N과 M （1）.java
@@ -1,42 +1,47 @@
-import java.util.Scanner;
-
+import java.io.*;
+import java.util.*;
 
 public class Main {
     public static int n, m;
-    public static boolean[] visit;
+    public static boolean[] chk;
     public static int[] ans;
+    public static StringBuilder sb;
 
     public static void dfs(int cnt) {
         if (cnt == m) {
             for (int i = 0; i < m; i++) {
-                System.out.print(ans[i] + " ");
-
-            }System.out.println();
+                sb.append(ans[i]);
+                sb.append(" ");
+            }
+            sb.append("\n");
             return;
         }
         for (int i = 1; i <= n; i++) {
-            if (!visit[i]) {
-                visit[i] = true;
+            if (!chk[i]) {
+                chk[i] = true;
                 ans[cnt] = i;
                 dfs(cnt + 1);
-                visit[i] = false;
+                chk[i] = false;
             }
         }
     }
 
     public static void solve() {
         dfs(0);
+        System.out.print(sb);
     }
 
-    public static void init() {
-        Scanner sc = new Scanner(System.in);
-        n = sc.nextInt();
-        m = sc.nextInt();
+    public static void init() throws Exception {
+        sb = new StringBuilder();
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        n = Integer.parseInt(st.nextToken());
+        m = Integer.parseInt(st.nextToken());
         ans = new int[m + 1];
-        visit = new boolean[n+1];
+        chk = new boolean[n + 1];
     }
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
         init();
         solve();
     }
