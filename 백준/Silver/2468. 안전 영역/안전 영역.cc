5f54cccfa2558a61579532e26c685fe4d38466b1
김수환 <101265039+genius00hwan@users.noreply.github.com>
2026-02-11 16:59:26 +0900

    [Silver I] Title: 안전 영역, Time: 16 ms, Memory: 2268 KB -BaekjoonHub

--- 백준/Silver/2468. 안전 영역/안전 영역.cc
+++ 백준/Silver/2468. 안전 영역/안전 영역.cc
@@ -1,65 +1,68 @@
-//
-// Created by 김수환 on 2022/08/21.
-//
-#include <iostream>
-#include <queue>
+#include <bits/stdc++.h>
 
 using namespace std;
 
-int n, top, rain, cnt, ans;
-int land[101][101];
+int n, top, cnt, ans;
+int graph[101][101];
 bool safe[101][101];
-bool visited[101][101];
-int xDir[4] = {0, 1, 0, -1};
-int yDir[4] = {1, 0, -1, 0};
+bool chk[101][101];
+int dr[4] = {0, 1, 0, -1};
+int dc[4] = {1, 0, -1, 0};
 
-void dfs(int x, int y) {
-    visited[x][y] = true;
+
+bool oob(int r, int c) {
+    return r < 0 || r >= n || c < 0 || c >= n;
+}
+
+void dfs(int r, int c) {
+    chk[r][c] = true;
 
     for (int i = 0; i < 4; i++) {
-        int nextX = x + xDir[i];
-        int nextY = y + yDir[i];
-        if (visited[nextX][nextY] || !safe[nextX][nextY]) continue;
-        if (0 <= nextX && nextX < n && 0 <= nextY && nextY < n)
-            dfs(nextX, nextY);
+        int nr = r + dr[i];
+        int nc = c + dc[i];
+        if (oob(nr, nc))continue;
+        if (chk[nr][nc] || !safe[nr][nc]) continue;
+        dfs(nr, nc);
     }
 }
 
-int main() {
-    ios::sync_with_stdio(false);
-    cin.tie(0);
-
+void init(){
     cin >> n;
     for (int i = 0; i < n; i++) {
         for (int j = 0; j < n; j++) {
-            cin >> land[i][j];
-            if (land[i][j] > top)
-                top = land[i][j];
+            cin >> graph[i][j];
+            top = max(top, graph[i][j]);
         }
     }
-    for (rain = 0; rain <= top; rain++) {//비 안올때 부터 다잠길 때 까지
+}
+
+int solve(){
+    for (int rain = 0; rain <= top; rain++) {
         cnt = 0;
-        for (int i = 0; i < n; i++) {//초기화
-            for (int j = 0; j < n; j++) {
-                visited[i][j] = false;
-                safe[i][j] = false;
-            }
-        }
         for (int i = 0; i < n; i++) {
-            for (int j = 0; j < n; j++)
-                safe[i][j] = (land[i][j] > rain);//강수량 보다 높은 땅이면 안전 지대
-        }
-        for (int i = 0; i < n; i++) {//안전영역 세기
             for (int j = 0; j < n; j++) {
-                if (safe[i][j] && !visited[i][j]) {
-                    dfs(i, j);
-                    cnt++;
-                }
+                chk[i][j] = false;
+                safe[i][j] = (graph[i][j] > rain);
             }
         }
-        if (ans < cnt)
-            ans = cnt;
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                if (!safe[i][j]) continue;
+                if (chk[i][j])continue;
+                dfs(i, j);
+                cnt++;
+            }
+        }
+        ans = max(ans, cnt);
     }
-    cout << ans;
+    return ans;
+}
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    init();
+    cout << solve();
     return 0;
 }
