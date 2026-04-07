8e47928b9e0c21f072b87b07cd2730801ef82398
김수환 <101265039+genius00hwan@users.noreply.github.com>
2026-04-07 12:28:22 +0900

    [Silver III] Title: 2×n 타일링, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Silver/11726. 2×n 타일링/2×n 타일링.cc
+++ 백준/Silver/11726. 2×n 타일링/2×n 타일링.cc
@@ -2,31 +2,23 @@
 
 using namespace std;
 
-#define MOD 10007
 int n;
-int mem[1002];
+int mem[1002] = {1, 2,};
 
-void init() {
-    cin >> n;
-    mem[0] = 1;
-    mem[1] = 2;
-}
 
 int dp(int i) {
     if (mem[i] != 0)return mem[i];
-    return mem[i] = (dp(i - 1) + dp(i - 2)) % MOD;
+    return mem[i] = (dp(i - 1) + dp(i - 2)) % 10'007;
 }
 
 void solve() {
-    cout << dp(n-1);
+    cin >> n;
+    cout << dp(n - 1);
 }
 
 int main() {
-    ios_base::sync_with_stdio(false);
+    ios::sync_with_stdio(0);
     cin.tie(nullptr);
-    cout.tie(nullptr);
 
-
-    init();
     solve();
 }
